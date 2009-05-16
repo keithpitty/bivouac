7c2d705073c70f60a28f2b25ea1a5c3a0b153afd
Martin Stannard <mstannard@gmail.com>
2009-05-16 18:48:51 +1000

    added some putative methods

--- bivouac.rb
+++ bivouac.rb
@@ -1,13 +1,37 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'active_record'
 
 helpers do
+
+  def write_vhost_conf
+    vhost = File.join('/var/www/bivouac/', name, '.conf')
+    File.open(vhost, 'w') do |f|
+      end
+  end
+
+  def init_repo
+  end
+
+  def add_post_commit(name)
+    File.join('/var/www/bivouac/', name, '.conf'
+    F
+  end
+
+  def restart_server
+  end
 
 end
 
 get '/' do
   haml :index
+end
+
+class Sites < ActiveRecord::Base
+
+
+
 end
 
 __END__
