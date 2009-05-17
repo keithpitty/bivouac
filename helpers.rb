fc938738f8d367c68922597865b97c4a945ed2ef
Martin Stannard <mstannard@gmail.com>
2009-05-17 12:38:28 +1000

    added public dir

--- helpers.rb
+++ helpers.rb
@@ -10,7 +10,7 @@ helpers do
 
   def init_repo(site)
     FileUtils.mkdir_p File.join(site.directory, 'tmp')
-    `touch #{File.join(site.directory, 'tmp', 'restart.txt')}"`
+    FileUtils.mkdir_p File.join(site.directory, 'public')
     current_dir = Dir.getwd
     Dir.chdir site.directory
     `git init`
